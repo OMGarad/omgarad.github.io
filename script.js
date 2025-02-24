c0d6ff675b1d9e28cf5bcb02c356c4331facae49
OMGarad <omgarad@gmail.com>
2025-02-24 18:27:15 -0500

    Modifying button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 async function fetchJoke() {
     const jokeContainer = document.querySelector('.joke-container');
     const refreshButton = document.getElementById('refresh-button');
+
     try {
         jokeContainer.innerHTML = `<p>Your joke is loading...</p>`;
         refreshButton.style.display = 'none';
@@ -9,14 +10,15 @@ async function fetchJoke() {
         const data = await response.json();
 
         jokeContainer.innerHTML = `<p>${data.value}</p>`;
-        refreshButton.style.display = 'block';
+        refreshButton.style.display = 'inline-block';
     } catch (error) {
         console.error('Error fetching joke:', error);
         jokeContainer.innerHTML = `<p>Failed to load joke. Please try again later.</p>`;
-        refreshButton.style.display = 'block';
+        refreshButton.style.display = 'inline-block';
     }
 }
 
 window.addEventListener('load', fetchJoke);
+
 const refreshButton = document.getElementById('refresh-button');
 refreshButton.addEventListener('click', fetchJoke);
